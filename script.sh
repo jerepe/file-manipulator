87313ae35a8a6fa37bd425de5dae3bd8d9ab8de7
jeremy <74825770+jerepe@users.noreply.github.com>
2022-05-16 17:58:30 +0200

    added folders, a file, and a text creation

--- script.sh
+++ script.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
+mkdir folder1 folder2
+echo "secret_line secret_word" > folder1/secretfile.txt
+
 # display a welcome message and store the user input into the variable "name":
 read -p "Hi, and Welcome. What is your name ? " name
-echo -e "Alright Mr.$name \U1F44D"
+echo -e "Alright Mr.$name"
 
-# read and store the first line of document changelog.md, containig the project's version number, in a variable:
-firstline=$(head -n1 ./folder1/changelog.md)
+# read and store the first line of document secretfile, containig the project's version number, in a variable:
+firstline=$(head -n1 ./folder1/secretfile.txt)
 
 # convert the variable firstline into an array "splitfirstline":
 read -a splitfirstline <<< $firstline
@@ -13,28 +16,37 @@ read -a splitfirstline <<< $firstline
 version=${splitfirstline[1]}
 
 # display an informational message and store the user input in a variable "continue":
-read -p "This program will copy all files from  folder1, into folder2. Please enter 1 if you wish to continue, and 0 to exit: " continue
+read -p "This program will copy all files from folder1, into folder2. Please enter 1 if you wish to continue, and 0 to exit: " continue
 
-# add conditions, and a nested loop, to either keep the program running to copy files from the source folder into the build folfer, or stop it using a goodbye message.
+# add conditions, and a nested loop, to either keep the program running to copy files from folder1 into folder2, or stop it using a goodbye message.
 # once done, will display folder2 content:
 if [ $continue == "1" ]
 then
   echo -e "\nOk. Processing files ...\n"
   for file in folder1/*
   do
-    if [ $file == source/secretinfo.md ]
+    if [ $file == folder1/secretfile.txt ]
     then
-      echo ">>> $file is being copied"
-      cp $file build/
-      sed -i'' -e 's/42/XX/g' build/secretinfo.md
+	echo ">>> $file is being copied"
+	echo ">>> $version is being replaced by XX for sharing purposes" 
+      cp $file folder2/
+      sed -n 's/secret_word/XX/g' folder2/secretfile.txt
+      # if you're on Mac, chances are that you need to see a new file named secretfile.txt-e, if you want to remove it, uncomment everything under this line:
+      for new_file in folder2/*
+      do
+          if [ $new_file == folder2/secretfile-e.txt ]
+	  then
+	      rm -r folder2/secretfile-e.txt
+	  fi
+      done
     else
       echo ">>> $file is being copied"
-      cp $file build/
+      cp $file folder2/
     fi
   done
   
-  echo -e "\nBuild version $version now contains the following files:\n"
-  ls ~/folder2
+  echo -e "\nfolder2 now contains the following file(s):\n"
+  echo ">>> $(ls ~/Documents/Gities/bashing/first_script/folder2)"
   echo""
   
 elif [ $continue == "0" ]
