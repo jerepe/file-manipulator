d6ad07d147f3ce1cdcedacb04644deb8ff112ae0
jeremy <74825770+jerepe@users.noreply.github.com>
2022-05-16 17:27:27 +0200

    comments added for clarification

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# First Script written from scratch
+# First Script written from scratch 
+
+### What does it do ?
+> You want to build a new project, you have your files stored in a folder named source.
+> You want to copy all files from folder1 to folder 2, except one thing: some content in the file named secretinfo.md needs to be hidden before sharing the newly created build folder.
+> Therefore, the program will hide this content.
+> If you want to replace any other content, simply replace the content of the sed command.
+> If you use a Mac, you'll probably need the extra  ** -i'' -e** options (sometimes it'll create, or not, a backup file. Don't forget to delete it!)
+> I added extra emoji to train myself on them.
+
+### How does it looks ?
+> ![result](video.mp4)
+
+### How do i use it ?
+> Program will ask for your name, then ask if you want to keep going or not. If you chose to do so, it'll go on with the previously explained task.
+
+### How was it done ?
+> Using Emacs, the CLI, and Bash.
+
+### Why ?
+> Needed to review some bash scripting concepts, so took on the quizz then the last project offered by CodeCademy to do so. Followed their plan, then changedsome of the content partially to train myself on some concepts.
--- script.sh
+++ script.sh
@@ -3,8 +3,8 @@
 read -p "Hi, and Welcome. What is your name ? " name
 echo -e "Alright Mr.$name \U1F44D"
 
-# read and store the first line of document changelog.md in a variable:
-firstline=$(head -n1 ./source/changelog.md)
+# read and store the first line of document changelog.md, containig the project's version number, in a variable:
+firstline=$(head -n1 ./folder1/changelog.md)
 
 # convert the variable firstline into an array "splitfirstline":
 read -a splitfirstline <<< $firstline
@@ -13,29 +13,34 @@ read -a splitfirstline <<< $firstline
 version=${splitfirstline[1]}
 
 # display an informational message and store the user input in a variable "continue":
-read -p "This program will copy all files from the source folder, into the build folder. Please enter 1 if you wish to continue, and 0 to exit: " continue
+read -p "This program will copy all files from  folder1, into folder2. Please enter 1 if you wish to continue, and 0 to exit: " continue
 
-# add conditions, and a nested loop, to either keep the program running to copy files from the source folder into the build folfer, or stop it using a goodbye message:
+# add conditions, and a nested loop, to either keep the program running to copy files from the source folder into the build folfer, or stop it using a goodbye message.
+# once done, will display folder2 content:
 if [ $continue == "1" ]
 then
   echo -e "\nOk. Processing files ...\n"
-  for filename in source/*
+  for file in folder1/*
   do
-    if [ $filename == source/secretinfo.md ]
+    if [ $file == source/secretinfo.md ]
     then
-      echo ">>> $filename is being copied"
-      cp $filename build/
+      echo ">>> $file is being copied"
+      cp $file build/
       sed -i'' -e 's/42/XX/g' build/secretinfo.md
     else
-      echo ">>> $filename is being copied"
-      cp $filename build/
+      echo ">>> $file is being copied"
+      cp $file build/
     fi
   done
+  
   echo -e "\nBuild version $version now contains the following files:\n"
-  cd ~/workspace/learn-bash-scripting-project/build; ls; cd ../
+  ls ~/folder2
+  echo""
+  
 elif [ $continue == "0" ]
 then
-  echo "Ok, come back when you are ready then. Bye"
+  echo "Please come back when you are ready. Bye"
+
 else
   echo "Error, try again"
 fi
